12bbb6724a06f71494684f2d685a6dd3377d8817
Matt Calimbas <matthewcalimbas@gmail.com>
2022-01-12 15:07:23 -0800

    added basic delete function

--- react-tasktraker/src/App.js
+++ react-tasktraker/src/App.js
@@ -27,13 +27,13 @@ const App = () => {
 // Delete Task
 
 const deleteTask = (id) => {
-  console.log(object)
+  setTasks(tasks.filter((task) => task.id !== id))
 }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
