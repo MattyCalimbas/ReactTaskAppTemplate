b80ae8da4b3f746a821f292dcd036a1336ac0a8d
Matt Calimbas <matthewcalimbas@gmail.com>
2022-01-13 11:50:13 -0800

    add more functionality to add task component

--- react-tasktraker/src/App.js
+++ react-tasktraker/src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState( [
     {
         id: 1,
@@ -25,8 +26,15 @@ const App = () => {
     },
 ])
 
-// Delete Task
 
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 1000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+// Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
@@ -39,7 +47,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show')}
     </div>
   );
